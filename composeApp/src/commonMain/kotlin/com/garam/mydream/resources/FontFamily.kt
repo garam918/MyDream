f6ebc6414b789b620bfc989a6173011a2a4253f7
imgaram <ijy918@naver.com>
2026-03-06 18:00:29 +0900

    feat : Font Family 정의

---
+++ composeApp/src/commonMain/kotlin/com/garam/mydream/resources/FontFamily.kt
@@ -0,0 +1,44 @@
+package com.garam.mydream.resources
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import mydream.composeapp.generated.resources.Res
+import mydream.composeapp.generated.resources.wanted_sans_bold
+import mydream.composeapp.generated.resources.wanted_sans_extra_bold
+import mydream.composeapp.generated.resources.wanted_sans_medium
+import mydream.composeapp.generated.resources.wanted_sans_regular
+import mydream.composeapp.generated.resources.wanted_sans_semi_bold
+import org.jetbrains.compose.resources.Font
+
+
+@Composable
+fun fontFamily() : FontFamily = FontFamily(
+
+    Font(
+        resource = Res.font.wanted_sans_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resource = Res.font.wanted_sans_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resource = Res.font.wanted_sans_semi_bold,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resource = Res.font.wanted_sans_bold,
+        weight = FontWeight.Bold
+    ),
+    Font(
+        resource = Res.font.wanted_sans_extra_bold,
+        weight = FontWeight.ExtraBold
+    ),
+//    Font(
+//        resource = Res.font,
+//        weight = FontWeight.Medium
+//    ),
+
+)
