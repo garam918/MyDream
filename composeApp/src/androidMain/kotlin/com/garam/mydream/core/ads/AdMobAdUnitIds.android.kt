7c14c1edf7d11119aae198133ce2848c2038b10f
imgaram <ijy918@naver.com>
2026-06-22 21:13:18 +0900

    feat: add Android home banner ad unit resolver

--- composeApp/src/androidMain/kotlin/com/garam/mydream/core/ads/AdMobAdUnitIds.android.kt
+++ composeApp/src/androidMain/kotlin/com/garam/mydream/core/ads/AdMobAdUnitIds.android.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.platform.LocalContext
 import app.lexilabs.basic.ads.AdUnitId
 import mydream.composeapp.generated.resources.Res
 import mydream.composeapp.generated.resources.admob_banner_ad_unit_id
+import mydream.composeapp.generated.resources.admob_home_banner_ad_unit_id
 import mydream.composeapp.generated.resources.admob_rewarded_ad_unit_id
 import org.jetbrains.compose.resources.stringResource
 
@@ -16,6 +17,15 @@ internal actual fun bannerAdUnitId(): String {
     }
 
     return stringResource(Res.string.admob_banner_ad_unit_id)
+}
+
+@Composable
+internal actual fun homeBannerAdUnitId(): String {
+    if (isDebuggableApp()) {
+        return AdUnitId.BANNER_DEFAULT
+    }
+
+    return stringResource(Res.string.admob_home_banner_ad_unit_id)
 }
 
 @Composable
