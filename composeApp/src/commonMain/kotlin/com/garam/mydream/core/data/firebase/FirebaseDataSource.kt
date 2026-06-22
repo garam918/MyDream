4a75d1a8bde797c9bdada8048a00fc5da1b859f8
imgaram <ijy918@naver.com>
2026-06-22 21:13:18 +0900

    feat: add Firebase dream report APIs

--- composeApp/src/commonMain/kotlin/com/garam/mydream/core/data/firebase/FirebaseDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/garam/mydream/core/data/firebase/FirebaseDataSource.kt
@@ -2,6 +2,7 @@ package com.garam.mydream.core.data.firebase
 
 import com.garam.mydream.core.database.UserDataEntity
 import com.garam.mydream.core.database.DreamAnalysisEntity
+import com.garam.mydream.core.database.DreamReportEntity
 
 interface FirebaseDataSource {
 
@@ -10,7 +11,11 @@ interface FirebaseDataSource {
 
     suspend fun saveDreamData(dreamData: DreamAnalysisEntity)
 
+    suspend fun saveDreamReportData(dreamReportEntity: DreamReportEntity)
+
     suspend fun getDreamData() : List<DreamAnalysisEntity>
+
+    suspend fun getDreamReportData() : List<DreamReportEntity>
 
     suspend fun deleteDreamData(id: String)
 
